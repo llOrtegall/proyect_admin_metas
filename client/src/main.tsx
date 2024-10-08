b09ae89af9635af4562f58eb5732738123c1fe9e
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-10-08 18:57:46 -0500

    test 2

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,10 @@
-import { RouterProvider } from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
-import router from './routes/index.tsx'
 import { StrictMode } from 'react'
-
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(<StrictMode><RouterProvider router={router} /></StrictMode>)
-
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
--- client/src/types/metas.d.ts
+++
@@ -1,6 +0,0 @@
-export interface Product {
-  id: number
-  producto: string
-  vta_dia: number
-  meta_dia: number
-}
