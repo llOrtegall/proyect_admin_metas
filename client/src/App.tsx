95203b7e5fe778a8ef39db30b78b95fa04d49794
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-10-08 19:24:05 -0500

    pareciera que funciona correctamente

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,51 @@
+import { DonutChart } from "./components/DonutChart"
+
+const chartdata = [
+  {
+    name: "SolarCells",
+    amount: 4890,
+  },
+  {
+    name: "Glass",
+    amount: 2103,
+  },
+  {
+    name: "JunctionBox",
+    amount: 2050,
+  },
+  {
+    name: "Adhesive",
+    amount: 1300,
+  },
+  {
+    name: "BackSheet",
+    amount: 1100,
+  },
+  {
+    name: "Frame",
+    amount: 700,
+  },
+  {
+    name: "Encapsulant",
+    amount: 200,
+  },
+]
+
+
 function App() {
   return (
-    <section className="h-screen w-screen flex items-center justify-center">
-      <h1>
-        Hello, world!
-      </h1>
+    <section className='h-screen w-screen flex items-center justify-center'>
+      <DonutChart
+        className="mx-auto"
+        data={chartdata}
+        category="name"
+        value="amount"
+        showLabel={true}
+        valueFormatter={(number: number) =>
+          `$${Intl.NumberFormat("us").format(number).toString()}`
+        }
+      />
+
     </section>
   )
 }
