6a179d09129b1465ee64b65b2b9bb4ac5ea391c7
llOrtegall <IvanOrtega_97@hotmail.com>
2024-10-21 08:45:13 -0500

    Refactor SucursalesPowerBi controller to add endpoint for retrieving a single sucursal by code

--- api/src/controllers/sucursalespowerbi.controller.ts
+++ api/src/controllers/sucursalespowerbi.controller.ts
@@ -15,3 +15,21 @@ export const getSucursalesPowerBi = async (req: Request, res: Response) => {
     res.status(500).json({ message: 'Error al obtener las sucursales' });
   }
 }
+
+export const getSucursalPowerBi = async (req: Request, res: Response) => {
+  try {
+    const { codigo } = req.params;
+    const sucursal = await SucursalesPowerBi.findOne({
+      where: { CODIGO: codigo }
+    })
+
+    if (!sucursal) {
+      return res.status(404).json({ message: 'No se encontró la sucursal' });
+    }
+
+    res.status(200).json(sucursal);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error al obtener la sucursal' });
+  }
+}
--- api/src/routes/sucursalespowerbi.routes.ts
+++ api/src/routes/sucursalespowerbi.routes.ts
@@ -1,6 +1,8 @@
-import { getSucursalesPowerBi } from '../controllers/sucursalespowerbi.controller';
+import { getSucursalesPowerBi, getSucursalPowerBi} from '../controllers/sucursalespowerbi.controller';
 import { Router } from 'express';
 
 export const RoutesPowerBi = Router();
 
 RoutesPowerBi.get('/sucursalesPB', getSucursalesPowerBi);
+
+RoutesPowerBi.get('/sucursalPB/:codigo', getSucursalPowerBi);
