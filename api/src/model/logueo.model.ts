4a95100a3d29eea6d9a5e8a1548bd6bc62d270d0
llOrtegall <IvanOrtega_97@hotmail.com>
2025-03-20 09:54:02 -0500

    feat: enhance Logueo model with additional fields and update database connection to PowerBI

--- api/src/model/logueo.model.ts
+++ api/src/model/logueo.model.ts
@@ -1,18 +1,24 @@
 import { Model, InferAttributes, InferCreationAttributes, DataTypes } from 'sequelize';
-import { con_db } from '../connections/index';
+import { PowerBI } from '../connections/powerbi';
 
 class Logueo extends Model<InferAttributes<Logueo>, InferCreationAttributes<Logueo>> {
-  declare username: string
-  declare sucursal: number
-  declare fecha_login: Date
+  declare username: string;
+  declare sucursal: number;
+  declare fecha_login: Date;
+  declare fechacreate?: Date;
+  declare fechaupdate?: Date;
+  declare version?: number;
 }
 
 Logueo.init({
-  username: { type: DataTypes.STRING, allowNull: false, primaryKey: true },
-  sucursal: { type: DataTypes.INTEGER, allowNull: false },
-  fecha_login: { type: DataTypes.DATE, allowNull: false }
+  username: { type: DataTypes.STRING(15), allowNull: false, primaryKey: true },
+  sucursal: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true },
+  fecha_login: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
+  fechacreate: { type: DataTypes.DATE, allowNull: true, defaultValue: DataTypes.NOW },
+  fechaupdate: { type: DataTypes.DATE, allowNull: true, defaultValue: DataTypes.NOW },
+  version: { type: DataTypes.INTEGER, allowNull: true, defaultValue: 0 }
 }, {
-  sequelize: con_db,
+  sequelize: PowerBI,
   tableName: 'HIST_USUARIOS_LOGUEADOS',
   timestamps: false
 });
