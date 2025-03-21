e178442595688d23067ce321e66ef9f3f9188a14
llOrtegall <IvanOrtega_97@hotmail.com>
2025-03-21 15:53:27 -0500

    validate query parameters for getLogueos endpoint and update logic to include empresa

--- api/src/controllers/logueo.controller.ts
+++ api/src/controllers/logueo.controller.ts
@@ -1,12 +1,25 @@
-import { getAllLogueos } from '../services/MySqlPower';
 import { Request, Response } from 'express';
+import { z } from 'zod';
+import { getAllLogueos } from '../services/MySqlPower';
+
+const schemaParams = z.object({
+  fecha: z.string().optional().default(''),
+  empresa: z.string().min(3).max(16)
+})
 
 export const getLogueos = async (req: Request, res: Response) => {
-  const fecha = req.query.fecha as string;
+  const params = req.query
+
+  const { success, data, error } = schemaParams.safeParse(params);
+
+  if (!success) {
+    return res.status(400).json({ message: error.format(), });
+  }
+
+  const { fecha, empresa } = data;
 
   try {
-    const rows = await getAllLogueos(fecha);
-
+    const rows = await getAllLogueos(fecha, empresa);
     res.status(200).json(rows);
   } catch (error) {
     console.error(error);
