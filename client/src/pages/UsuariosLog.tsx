b0748a78de7c55e99f225ee00753e173e8c2f0b9
llOrtegall <IvanOrtega_97@hotmail.com>
2025-03-21 15:57:37 -0500

    refactor UsuariosLogueados component to use custom hook for logueo data and improve filtering functionality

--- client/src/pages/UsuariosLog.tsx
+++ client/src/pages/UsuariosLog.tsx
@@ -1,43 +1,32 @@
+import { HistorialLogin } from '../components/exports/HistorialLogin';
 import { Table, TableBody, TableCell, TableHead, TableHeaderCell, TableRow } from '../components/Table';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { URL_API_DATA } from '../utils/constants';
-
-export interface Logueo {
-  SUCURSAL: number;
-  NOMBRES: string;
-  DOCUMENTO: string;
-  NOMBRECARGO: string;
-  FECHACREATE: string;
-  FECHAUPDATE: string;
-}
+import { useLogueos } from '../hooks/useLogueos';
+import { useState } from 'react';
 
 function UsuariosLogueados() {
-  const [logueo, setLogueo] = useState<Logueo[]>([]);
   const [fecha, setFecha] = useState<string>('');
   const [filter, setFilter] = useState<string>('');
-
-  useEffect(() => {
-    axios.get(`${URL_API_DATA}/logueo?fecha=${fecha}`)
-      .then(response => setLogueo(response.data))
-      .catch(error => console.log(error));
-  }, [fecha]);
-
-  const dataFiltered = logueo.filter(log => log.SUCURSAL.toString().includes(filter))
+  const dataFiltered = useLogueos(fecha, filter);
 
   return (
     <section className='text-black'>
       <div className='flex items-center justify-around bg-slate-200 px-4 py-2 rounded-t-md'>
         <h1 className='text-sm 2xl:text-lg 3xl:text-2xl font-bold'>Usuarios Logueados</h1>
         <div>
-          <label className='text-xs 2xl:text-sm 3xl:text-base '>Sucursal:</label>
-          <input type='search' className='text-xs 2xl:text-sm 3xl:text-base border border-gray-300 rounded-md' placeholder='N° Sucursal' value={filter} onChange={ev => setFilter(ev.target.value)} />
+          <label className='text-xs 2xl:text-sm 3xl:text-base '>Filtrar:</label>
+          <input
+            onChange={ev => setFilter(ev.target.value)}
+            placeholder='39856 | Pepita Perez | 11182****'
+            value={filter}
+            type='search'
+            className='text-xs 2xl:text-sm 3xl:text-base border border-gray-300 rounded-md w-96'
+          />
         </div>
         <form>
           <label className='text-xs 2xl:text-sm 3xl:text-base '>Fecha:</label>
           <input type='date' className='text-xs 2xl:text-sm 3xl:text-base border border-gray-300 rounded-md' value={fecha} onChange={ev => setFecha(ev.target.value)} />
         </form>
-        {/* <ExcelData datos={sugeridos} /> */}
+        <HistorialLogin datos={dataFiltered} /> 
       </div>
       <div className='h-[92vh] overflow-y-auto'>
         <Table>
@@ -45,7 +34,7 @@ function UsuariosLogueados() {
             <TableRow>
               <TableHeaderCell>Sucursal</TableHeaderCell>
               <TableHeaderCell>Documento</TableHeaderCell>
-              <TableHeaderCell>Nnombres</TableHeaderCell>
+              <TableHeaderCell>Nombres</TableHeaderCell>
               <TableHeaderCell>Cargo</TableHeaderCell>
               <TableHeaderCell>Hora Primer Login</TableHeaderCell>
               <TableHeaderCell>Hora Ultimo Login</TableHeaderCell>
